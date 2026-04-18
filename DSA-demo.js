4b5be6bfb353f014803c0283c74e13bfdfcf4410
Langmiao <langkoumiao@outlook.com>
2026-04-18 21:42:57 +1000

    Improve demo user interface and add SLH-DSA content

---
+++ DSA-demo.js
@@ -0,0 +1,65 @@
+const crypto = require('node:crypto');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+var publicKey = '';
+
+app.post('/login', (req, res) => {
+    const { username, password, algorithm } = req.body;
+
+    if (username === 'admin' && password === '123456') {
+        const payload = {
+            username: username,
+            password: password
+        };
+        const data = Buffer.from(JSON.stringify(payload));
+
+        if (algorithm === 'ml') {
+            const { publicKey, privateKey } = crypto.generateKeyPairSync('ml-dsa-65');
+            this.publicKey = publicKey;
+            const signature = crypto.sign(null, data, privateKey);
+
+            res.json({
+                payload: payload,
+                signature: signature.toString('base64')
+            }).send();
+        } else if (algorithm === 'slh') {
+            const { publicKey, privateKey } = crypto.generateKeyPairSync('slh-dsa-sha2-128s');
+            this.publicKey = publicKey;
+            const signature = crypto.sign(null, data, privateKey);
+
+            res.json({
+                payload: payload,
+                signature: signature.toString('base64')
+            }).send();
+        } else res.status(204).send();
+    }
+
+
+
+});
+
+app.post('/protected', (req, res) => {
+    const { payload, signature } = req.body;
+
+    if (!payload || !signature) {
+        return res.status(400).send();
+    }
+
+    const dataToVerify = Buffer.from(JSON.stringify(payload));
+    const signatureBuffer = Buffer.from(signature, 'base64');
+
+    const isValid = crypto.verify(null, dataToVerify, this.publicKey, signatureBuffer);
+
+    if (isValid) {
+        res.status(200).send("Verified!");
+    } else {
+        res.status(403).send();
+    }
+});
+
+app.listen(3000, () => console.log("http://localhost:3000"));
--- test_openssl.js
+++
@@ -1,3 +0,0 @@
-const crypto = require('node:crypto');
-const {publicKey, privateKey} = crypto.generateKeyPairSync('ml-dsa-65');
-console.log("PQC Key generated.");
